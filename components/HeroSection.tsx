31dfd4c7c1718ddca8d982e3895aa673a769580b
zeaforx <ajibcaleb@gmail.com>
2024-12-24 01:27:06 +1300

    updates

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -100,10 +100,10 @@ const HeroSection = () => {
                     transition={{ duration: 0.5 }}
                     className="col-span-4 place-self-center mt-4 lg:mt-0"
                 >
-                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <div className="overflow-hidden rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                         <p>blown apart</p>
                         <Image
-                            src="/adhp-1.png"
+                            src="/hero-img.png"
                             alt="hero image"
                             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             width={300}
